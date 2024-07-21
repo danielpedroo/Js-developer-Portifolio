79a51662d44fdcb7bf10ff61844cfd35cb47ee76
danielpedroo <danielpapereiraJV@gmail.com>
2024-07-20 22:14:17 -0300

    Update Files - Refactor Docs

--- Js/api.js
+++ Js/api.js
@@ -1,7 +1,7 @@
 
 //Request data Json;
 async function fetchProfileData(){
-    const urlJson = "https://raw.githubusercontent.com/danielpedroo/Js-developer-Portifolio/main/Src/Json/profile.json";
+    const urlJson = "https://raw.githubusercontent.com/danielpedroo/Js-developer-Portifolio/main/Json/profile.json";
     const fetching = await fetch (urlJson);
     return fetching.json();
 }
